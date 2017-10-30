452075ffcd72e49071f1078b28d9109ee8bbb778
Lam <2006220@student.acalanes.k12.ca.us>
2017-10-30 10:57:02 -0700

    Student

---
+++ Student.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Student here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Student
+{
+   private int idNumber;
+   private String name;
+   private Student bestFriend;
+   
+   
+   public Student() {
+    }
+    
+   public Student (int num, String nm) {
+       this.idNumber = num;
+       this.name = nm;
+    }
+   public String getName() {
+       return this.name;
+    }
+    public int combineIDs() {
+        return this.idNumber + bestFriend.idNumber;
+    }
+    public Student makeStudent() {
+        Student joe = new Student(this.idNumber, bestFriend.name);
+        return joe;
+    }
+    public String toString() {
+        return "The student name is: " + this.name;
+    }
+}
